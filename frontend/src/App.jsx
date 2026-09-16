66e1f5ddbbad39275038fb081031080d369d32a5
Sachin Kumar <sachin80510@gmail.com>
2026-09-16 21:09:50 +0530

    feat: create root App component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
+import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <div className="min-h-screen">
+            <AppRoutes />
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
